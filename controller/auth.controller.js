e597310f2322a70c92c617e49d21aa31d48103d9
somcpp <saubhagya.sharma3.14@gmail.com>
2025-08-30 08:33:39 +0530

    create signin api

---
+++ controller/auth.controller.js
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import {errorHandler} from '../utils/error.js'
+export const signup = async (req, res, next) => {
+  const { username, email, password } = req.body;
+  const hashedPassword = bcrypt.hashSync(password, 10);
+  const newUser = new User({ username, email, password: hashedPassword });
+  try {
+    await newUser.save();
+    res.status(201).json("User created successfully!");
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validUser = await User.findOne({email});
+    if (!validUser) return next(errorHandler(404, "User not found"));
+    const validPassword = bcrypt.compareSync(password, validUser.password);
+    if (!validPassword) return next(errorHandler(401, "Invalid Password"));
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const {password: pass,...rest} = validUser._doc
+    res
+      .cookie("access_token", token, { httpOnly: true })
+      .status(200)
+      .json(rest);
+  } catch (error) {
+    next(error);
+  }
+};
--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 import express from 'express'
 import mongoose from 'mongoose';
 import dotenv from 'dotenv'
+import authRouter from './routes/auth.route.js'
 
 dotenv.config()
 const app = express();
 
+app.use((err,req,res,next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+    return res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message
+    })
+})
+
+app.use(express.json());
 
 mongoose.connect(process.env.MONGO).then(() => {
     console.log('connected to database');
@@ -17,3 +29,9 @@ mongoose.connect(process.env.MONGO).then(() => {
 app.listen(3000, () => {
     console.log("server is running on port 3000")
 })
+
+app.get('/test', (req,res) => {
+    res.send("Hello World")
+})
+
+app.use('/api/auth',authRouter);
