6c7f28390374136c641e31b1d01e109c251d0d25
somcpp <saubhagya.sharma3.14@gmail.com>
2025-08-31 12:53:34 +0530

    add google auth

--- controller/auth.controller.js
+++ controller/auth.controller.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
-import {errorHandler} from '../utils/error.js'
+import { errorHandler } from "../utils/error.js";
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
   const hashedPassword = bcrypt.hashSync(password, 10);
@@ -18,17 +18,44 @@ export const signup = async (req, res, next) => {
 export const signin = async (req, res, next) => {
   const { email, password } = req.body;
   try {
-    const validUser = await User.findOne({email});
+    const validUser = await User.findOne({ email });
     if (!validUser) return next(errorHandler(404, "User not found"));
     const validPassword = bcrypt.compareSync(password, validUser.password);
     if (!validPassword) return next(errorHandler(401, "Invalid Password"));
     const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
-    const {password: pass,...rest} = validUser._doc
+    const { password: pass, ...rest } = validUser._doc;
     res
       .cookie("access_token", token, { httpOnly: true })
       .status(200)
       .json(rest);
   } catch (error) {
     next(error);
+  }
+};
+
+export const google = async (req, res, next) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    if (user) {
+      const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+      const { password: pass, ...rest } = user._doc;
+      res
+        .cookie('access_token',token, {httpOnly: true})
+        .status(200)
+        .json(rest)
+    } else {
+      const generatedPassword = Math.random().toString(36).slice(-8);
+      const hashedPassword = bcrypt.hashSync(generatedPassword,10);
+      const newUser = new User({username: req.body.name.split(" ").join("").toLowerCase(), email: req.body.email, password: hashedPassword, avatar: req.body.photo});
+      await newUser.save();
+      const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const { password: pass, ...rest } = validUser._doc;
+    res
+      .cookie("access_token", token, { httpOnly: true })
+      .status(200)
+      .json(rest);
+    }
+  } catch (error) {
+    next(error)
   }
 };
--- index.js
+++ index.js
@@ -6,15 +6,7 @@ import authRouter from './routes/auth.route.js'
 dotenv.config()
 const app = express();
 
-app.use((err,req,res,next) => {
-    const statusCode = err.statusCode || 500;
-    const message = err.message || 'Internal Server Error';
-    return res.status(statusCode).json({
-        success: false,
-        statusCode,
-        message
-    })
-})
+
 
 app.use(express.json());
 
@@ -28,10 +20,23 @@ mongoose.connect(process.env.MONGO).then(() => {
 
 app.listen(3000, () => {
     console.log("server is running on port 3000")
-})
+});
 
-app.get('/test', (req,res) => {
-    res.send("Hello World")
-})
+// Place all routes above this
+// Example route
+app.get('/test', (req, res) => {
+    res.send("Hello World");
+});
 
-app.use('/api/auth',authRouter);
+app.use('/api/auth', authRouter);
+
+// Error handler should be the last middleware
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+    return res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message
+    });
+});
