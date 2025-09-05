20f4e20d8d64e8b910c5337fd595e95a5be82e09
somcpp <saubhagya.sharma3.14@gmail.com>
2025-09-05 16:00:56 +0530

    create sign-out api

--- controller/auth.controller.js
+++ controller/auth.controller.js
@@ -59,3 +59,12 @@ export const google = async (req, res, next) => {
     next(error)
   }
 };
+
+export const signout = async (req,res,next) => {
+  try{
+    res.clearCookie('access_token');
+    res.status(200).json('user has been logged out!');
+  } catch(error) {
+    next(error);
+  }
+}
