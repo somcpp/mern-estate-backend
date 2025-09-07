c6b0f35f4cc1273f6834d8ea8351b3b3b61402f2
somcpp <saubhagya.sharma3.14@gmail.com>
2025-09-07 22:02:59 +0530

    create listing update api

--- routes/listing.route.js
+++ routes/listing.route.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import { verifyToken } from '../utils/verifyUser.js';
-import { createListing, deleteListing } from '../controller/listing.controller.js';
+import { createListing, deleteListing, updateUserListings,getListing } from '../controller/listing.controller.js';
 
 const router = express.Router();
 
 router.post('/create', verifyToken, createListing);
 router.delete('/delete/:id',verifyToken,deleteListing)
+router.post('/update/:id', verifyToken, updateUserListings)
+router.get('/get/:id', getListing);
 
 export default router;
